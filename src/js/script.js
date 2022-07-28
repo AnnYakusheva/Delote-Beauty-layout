f9ee39524acc6865cbaa53c81f1de0d44ad74c55
AnnYakusheva <59803594+AnnYakusheva@users.noreply.github.com>
2022-07-28 16:39:54 +0700

    Entered changes

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,6 @@ const burgerButton  = document.getElementById('header__burger');
 const burgerMenu = document.getElementById('burger')
 const burgerClose = document.getElementById('burger__close')
 
-console.log(burgerMenu)
-
 burgerButton.addEventListener('click', () => {
     burgerMenu.classList.add('display-block');
 })
